6b3e68e17d0ef224b19af7135ec717826992354d
Victor Campos <victorboaventcampos@gmail.com>
2020-06-03 10:06:13 -0300

    Beta
    Version beta of Portal2

---
+++ app.js
@@ -0,0 +1,42 @@
+const API_KEY = 'e86b7c2ec9584cff9bf3eeb802515300';
+
+function exibeNoticias() {
+    let divTela = document.getElementById('tela');
+    let texto = '';
+
+    // Montar texto HTML das noticias
+    let dados = JSON.parse(this.responseText);
+    for (i = 0; i < dados.articles.length; i++) {
+        let noticia = dados.articles[i];
+        let data = new Date(noticia.publishedAt);
+
+        texto = texto + `
+        <div class="col-lg-12">
+            <div class="box-noticia">
+                <img src="${noticia.urlToImage}" alt="">
+                <span class="titulo">${noticia.title}</span><br>
+                <span class="creditos">${data.toLocaleDateString ()} - 
+                    ${noticia.source.name} - 
+                    ${noticia.author}</span><br>
+                <span class="text">
+                ${noticia.content} <a href="${noticia.url}">Leia mais ...</a>
+                </span>
+            </div>
+            </div>            
+        `;
+    };
+
+    // Preencher a DIV com o texto HTML
+    divTela.innerHTML = texto;
+}
+
+function executaPesquisa() {
+    let query = document.getElementById('txtPesquisa').value;
+
+    let xhr = new XMLHttpRequest();
+    xhr.onload = exibeNoticias;
+    xhr.open('GET', `http://newsapi.org/v2/everything?q=${query}&apiKey=${API_KEY}`);
+    xhr.send();
+}
+
+document.getElementById('btnPesquisa').addEventListener('click', executaPesquisa);
