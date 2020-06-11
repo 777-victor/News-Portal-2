bc29296598785b9e2d69968092f5db3edda841fa
Victor Campos <victorboaventcampos@gmail.com>
2020-06-11 20:03:00 -0300

    update

--- script/app.js
+++ script/app.js
@@ -41,3 +41,34 @@ function executaPesquisa() {
 }
 
 document.getElementById('btnPesquisa').addEventListener('click', executaPesquisa);
+
+onload = () => {
+    let aside = '';
+    aside = `<div class="main-search" id="some">
+    <form action="" id="nav-main-search">
+        <button id="btnPesquisa"> <svg focusable="false" viewBox="0 0 512 512" class="svg-inline--fa fa-search fa-w-1search 6 fa-fw">
+   <path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z" class=""></path></svg>
+</button>
+
+
+        <input id="txtPesquisa" type="text" class="search-input-field" placeholder="Pesquisar">
+    </form>
+</div>
+<ul class="nav flex-column">
+    <li class="nav-item">
+        <a class="nav-link active" href="#">Active</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+    </li>
+</ul>`;
+    let navegacao = document.getElementById('navbarNav');
+    document.querySelector('.navbar-toggler').onclick = () => navegacao.innerHTML = aside;
+    console.log(aside);
+}
